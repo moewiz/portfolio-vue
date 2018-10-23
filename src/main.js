8aec43cda31aa0685539c6d7137877f183f92699
Moe Wiz <meo.wizard@gmail.com>
2018-10-23 22:04:18 +0700

    Init simple Steps contact

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
+import { Button, Steps, message, notification } from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
 import App from './App.vue'
 
+Vue.component(Steps.name, Steps)
+Vue.component(Steps.Step.name, Steps.Step)
+Vue.component(Button.name, Button)
 Vue.config.productionTip = false
+Vue.prototype.$message = message
+Vue.prototype.$notification = notification
 
 new Vue({
   render: h => h(App)
